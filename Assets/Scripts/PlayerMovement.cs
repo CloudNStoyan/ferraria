0df296ab76afb5b976762394f5ae37bb871dd327
Stoyan <slownessbg@gmail.com>
2018-07-06 02:39:44 +0300

    Adding key features

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,16 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	// Use this for initialization
+
+
     private Rigidbody2D rb;
+
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool onGround;
+    public double speed;
 
     private float x;
     private float y;
@@ -17,31 +26,39 @@ public class PlayerMovement : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		rb.velocity = new Vector2(x,y);
+    void Update()
+    {
+        rb.velocity = new Vector2(x, y);
+        speed = rb.velocity.x;
 
-	    if (Input.GetKeyDown(KeyCode.A))
-	    {
+        onGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
             Left();
-	    } else if (Input.GetKeyDown(KeyCode.D))
-	    {
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
             Right();
-	    } else if (Input.GetKeyDown(KeyCode.Space))
-	    {
+        } else if (Input.GetKeyDown(KeyCode.Space))
+        {
             Jump();
-	    } else if (Input.GetKeyUp(KeyCode.Space))
-	    {
-            Move(0);
-	    } else if (Input.GetKeyDown(KeyCode.R))
-	    {
-            Move(-10);
-	    }
+        }
 
-	    if (!Input.anyKey)
-	    {
-            Move(0,0);
-	    }
-	}
+        if (!Input.anyKey)
+        {
+            Move(0, 0);
+        }
+
+    }
+    void Jump()
+    {
+        if (onGround)
+        {
+            float percantage = (rb.velocity.x / 100) * 5;
+            rb.velocity = new Vector2(rb.velocity.x - 3, rb.velocity.y + 50);
+        }
+    }
 
     void Move(float x, float y)
     {
@@ -49,23 +66,13 @@ public class PlayerMovement : MonoBehaviour {
         this.y = y;
     }
 
-    void Move(float y)
-    {
-        this.y = y;
-    }
-
     void Left()
     {
-        Move(-10,this.rb.velocity.y);
+        Move(-10,0);
     }
 
     void Right()
     {
-        Move(10, this.rb.velocity.y);
-    }
-
-    void Jump()
-    {
-        Move(this.rb.velocity.x,10);
+        Move(10, 0);
     }
 }
